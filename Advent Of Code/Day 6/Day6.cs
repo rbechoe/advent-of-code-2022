347f2505716c3c4b195725fe9e403dfce7378405
Ravi Bechoe <ravibechoe@outlook.com>
2022-12-06 11:49:30 +0100

    day 6 completed

---
+++ Advent Of Code/Day 6/Day6.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode
+{
+    public class Day6
+    {
+        string[] lines;
+        int containCount = 0;
+        int bufferSize = 14; // 4 = part 1, 14 = part 2
+
+        public Day6()
+        {
+            lines = File.ReadAllLines("F:\\HKU\\Jaar 3\\advent-of-code-2022\\Advent Of Code\\Day 6\\day6.txt"); //pc
+            //lines = File.ReadAllLines("C:\\Users\\rbech\\HKU\\advent-of-code-2022\\Advent Of Code\\Day 6\\day6.txt"); //laptop
+
+            Part1();
+        }
+
+        ~Day6()
+        {
+
+        }
+
+        private void Part1()
+        {
+            Queue<string> buffer = new Queue<string>();
+            for (int i = 0; i < lines[0].Count(); i++)
+            {
+                containCount++;
+                buffer.Enqueue(lines[0][i].ToString());
+                if (buffer.Count > bufferSize) buffer.Dequeue();
+                if (buffer.Count > bufferSize - 1 && LetterCheck(buffer))
+                {
+                    break;
+                }
+            }
+        }
+
+        private bool LetterCheck(Queue<string> buffer)
+        {
+            string[] bufferCol = buffer.ToArray();
+
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                if (i != buffer.Count - 1)
+                {
+                    for (int j = i + 1; j < buffer.Count; j++)
+                    {
+                        if (bufferCol[i] == bufferCol[j])
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Done: " + containCount);
+        }
+    }
+}
--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -6,11 +6,12 @@
         //private static Day2 day2 = new Day2();
         //private static Day3 day3 = new Day3();
         //private static Day4 day4 = new Day4();
-        private static Day5 day5 = new Day5();
+        //private static Day5 day5 = new Day5();
+        private static Day6 day6 = new Day6();
 
         public static void Main()
         {
-            day5.PrintResult();
+            day6.PrintResult();
         }
     }
 }
