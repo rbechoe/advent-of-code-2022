e5438fa01e59cf8d66046132eede04db87c296f2
Ravi Bechoe <ravibechoe@outlook.com>
2022-12-08 16:30:48 +0100

    day 8 part 1 done, part 2 bugged

---
+++ Advent Of Code/Day 8/Day8.cs
@@ -0,0 +1,168 @@
+﻿namespace AdventOfCode
+{
+    public class Day8
+    {
+        string[] lines;
+
+        List<List<int>> forrest = new List<List<int>>();
+
+        int visibleCount = 0;
+
+        public Day8()
+        {
+            //lines = File.ReadAllLines("F:\\HKU\\Jaar 3\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //pc
+            lines = File.ReadAllLines("C:\\Users\\rbech\\HKU\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //laptop
+
+            foreach (string line in lines)
+            {
+                forrest.Add(new List<int>());
+                for (int i = 0; i < line.Length; i++)
+                {
+                    forrest[forrest.Count - 1].Add(int.Parse(line[i].ToString()));
+                }
+            }
+
+            //Part1();
+            Part2();
+        }
+
+        ~Day8()
+        {
+
+        }
+
+        private void Part1()
+        {
+            // all edge numbers are visible
+            // start and end numbers per int list
+            // tree can be visible per direction (top, bottom, right, left)
+            // tree is invisible if same height is blocking
+            for (int column = 0; column < forrest.Count; column++)
+            {
+                for (int row = 0; row < forrest[column].Count; row++)
+                {
+                    if (column == 0 || row == 0 || column == forrest.Count - 1 || row == forrest[column].Count - 1)
+                    {
+                        visibleCount++;
+                    }
+                    else
+                    {
+                        // if any side visible, continue to next tree
+                        int count = 4;
+                        // check left side
+                        for (int i = 0; i < row; i++)
+                        {
+                            if (forrest[column][row] <= forrest[column][i])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                        // check right side
+                        for (int i = row + 1; i < forrest[column].Count; i++)
+                        {
+                            if (forrest[column][row] <= forrest[column][i])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                        // check top side
+                        for (int i = 0; i < column; i++)
+                        {
+                            if (forrest[column][row] <= forrest[i][row])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+                        // check bottom side
+                        for (int i = column + 1; i < forrest.Count; i++)
+                        {
+                            if (forrest[column][row] <= forrest[i][row])
+                            {
+                                count--;
+                                break;
+                            }
+                        }
+
+                        if (count > 0)
+                        {
+                            visibleCount++;
+                        }
+                    }
+                }
+            }
+        }
+
+        void Part2()
+        {
+            for (int column = 0; column < forrest.Count; column++)
+            {
+                for (int row = 0; row < forrest[column].Count; row++)
+                {
+                    if (column == 0 || row == 0 || column == forrest.Count - 1 || row == forrest[column].Count - 1)
+                    {
+                        //visibleCount++;
+                    }
+                    else
+                    {
+                        // if any side visible, continue to next tree
+                        int leftScore = 0, rightScore = 0, topScore = 0, bottomScore = 0;
+
+                        // check left side
+                        for (int i = row - 1; i > 0; i--)
+                        {
+                            leftScore++;
+                            if (forrest[column][row] <= forrest[column][i])
+                            {
+                                break;
+                            }
+                        }
+                        // check right side
+                        for (int i = row + 1; i < forrest[column].Count; i++)
+                        {
+                            rightScore++;
+                            if (forrest[column][row] <= forrest[column][i])
+                            {
+                                break;
+                            }
+                        }
+                        // check top side
+                        for (int i = column - 1; i > 0; i--)
+                        {
+                            topScore++;
+                            if (forrest[column][row] <= forrest[i][row])
+                            {
+                                break;
+                            }
+                        }
+                        // check bottom side
+                        for (int i = column + 1; i < forrest.Count; i++)
+                        {
+                            bottomScore++;
+                            if (forrest[column][row] <= forrest[i][row])
+                            {
+                                break;
+                            }
+                        }
+
+                        int totalScore = leftScore * rightScore * topScore * bottomScore;
+                        if (totalScore > visibleCount)
+                        {
+                            Console.WriteLine(leftScore + " - " + rightScore + " - " + topScore + " - " + bottomScore);
+                            Console.WriteLine(column + " - " + row);
+                            Console.WriteLine(forrest[column][row]);
+                            visibleCount = totalScore;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Done: " + visibleCount);
+        }
+    }
+}
--- Advent Of Code/Program.cs
+++ Advent Of Code/Program.cs
@@ -8,11 +8,12 @@
         //private static Day4 day4 = new Day4();
         //private static Day5 day5 = new Day5();
         //private static Day6 day6 = new Day6();
-        private static Day7 day7 = new Day7();
+        //private static Day7 day7 = new Day7();
+        private static Day8 day8 = new Day8();
 
         public static void Main()
         {
-            day7.PrintResult();
+            day8.PrintResult();
         }
     }
 }
