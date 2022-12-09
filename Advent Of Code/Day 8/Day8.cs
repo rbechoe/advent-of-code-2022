b82ce98729ff389b7d8e14608e78638ee1d3cd86
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-12-09 01:12:14 +0100

    day 8 fixed I was dum dum

--- Advent Of Code/Day 8/Day8.cs
+++ Advent Of Code/Day 8/Day8.cs
@@ -4,21 +4,21 @@
     {
         string[] lines;
 
-        List<List<int>> forrest = new List<List<int>>();
+        List<List<int>> forest = new List<List<int>>();
 
         int visibleCount = 0;
 
         public Day8()
         {
-            //lines = File.ReadAllLines("F:\\HKU\\Jaar 3\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //pc
-            lines = File.ReadAllLines("C:\\Users\\rbech\\HKU\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //laptop
+            lines = File.ReadAllLines("F:\\HKU\\Jaar 3\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //pc
+            //lines = File.ReadAllLines("C:\\Users\\rbech\\HKU\\advent-of-code-2022\\Advent Of Code\\Day 8\\day8.txt"); //laptop
 
             foreach (string line in lines)
             {
-                forrest.Add(new List<int>());
+                forest.Add(new List<int>());
                 for (int i = 0; i < line.Length; i++)
                 {
-                    forrest[forrest.Count - 1].Add(int.Parse(line[i].ToString()));
+                    forest[forest.Count - 1].Add(int.Parse(line[i].ToString()));
                 }
             }
 
@@ -37,11 +37,11 @@
             // start and end numbers per int list
             // tree can be visible per direction (top, bottom, right, left)
             // tree is invisible if same height is blocking
-            for (int column = 0; column < forrest.Count; column++)
+            for (int column = 0; column < forest.Count; column++)
             {
-                for (int row = 0; row < forrest[column].Count; row++)
+                for (int row = 0; row < forest[column].Count; row++)
                 {
-                    if (column == 0 || row == 0 || column == forrest.Count - 1 || row == forrest[column].Count - 1)
+                    if (column == 0 || row == 0 || column == forest.Count - 1 || row == forest[column].Count - 1)
                     {
                         visibleCount++;
                     }
@@ -52,16 +52,16 @@
                         // check left side
                         for (int i = 0; i < row; i++)
                         {
-                            if (forrest[column][row] <= forrest[column][i])
+                            if (forest[column][row] <= forest[column][i])
                             {
                                 count--;
                                 break;
                             }
                         }
                         // check right side
-                        for (int i = row + 1; i < forrest[column].Count; i++)
+                        for (int i = row + 1; i < forest[column].Count; i++)
                         {
-                            if (forrest[column][row] <= forrest[column][i])
+                            if (forest[column][row] <= forest[column][i])
                             {
                                 count--;
                                 break;
@@ -70,16 +70,16 @@
                         // check top side
                         for (int i = 0; i < column; i++)
                         {
-                            if (forrest[column][row] <= forrest[i][row])
+                            if (forest[column][row] <= forest[i][row])
                             {
                                 count--;
                                 break;
                             }
                         }
                         // check bottom side
-                        for (int i = column + 1; i < forrest.Count; i++)
+                        for (int i = column + 1; i < forest.Count; i++)
                         {
-                            if (forrest[column][row] <= forrest[i][row])
+                            if (forest[column][row] <= forest[i][row])
                             {
                                 count--;
                                 break;
@@ -97,11 +97,11 @@
 
         void Part2()
         {
-            for (int column = 0; column < forrest.Count; column++)
+            for (int column = 0; column < forest.Count; column++)
             {
-                for (int row = 0; row < forrest[column].Count; row++)
+                for (int row = 0; row < forest[column].Count; row++)
                 {
-                    if (column == 0 || row == 0 || column == forrest.Count - 1 || row == forrest[column].Count - 1)
+                    if (column == 0 || row == 0 || column == forest.Count - 1 || row == forest[column].Count - 1)
                     {
                         //visibleCount++;
                     }
@@ -111,37 +111,37 @@
                         int leftScore = 0, rightScore = 0, topScore = 0, bottomScore = 0;
 
                         // check left side
-                        for (int i = row - 1; i > 0; i--)
+                        for (int i = row - 1; i >= 0; i--)
                         {
                             leftScore++;
-                            if (forrest[column][row] <= forrest[column][i])
+                            if (forest[column][row] <= forest[column][i])
                             {
                                 break;
                             }
                         }
                         // check right side
-                        for (int i = row + 1; i < forrest[column].Count; i++)
+                        for (int i = row + 1; i < forest[column].Count; i++)
                         {
                             rightScore++;
-                            if (forrest[column][row] <= forrest[column][i])
+                            if (forest[column][row] <= forest[column][i])
                             {
                                 break;
                             }
                         }
                         // check top side
-                        for (int i = column - 1; i > 0; i--)
+                        for (int i = column - 1; i >= 0; i--)
                         {
                             topScore++;
-                            if (forrest[column][row] <= forrest[i][row])
+                            if (forest[column][row] <= forest[i][row])
                             {
                                 break;
                             }
                         }
                         // check bottom side
-                        for (int i = column + 1; i < forrest.Count; i++)
+                        for (int i = column + 1; i < forest.Count; i++)
                         {
                             bottomScore++;
-                            if (forrest[column][row] <= forrest[i][row])
+                            if (forest[column][row] <= forest[i][row])
                             {
                                 break;
                             }
@@ -150,9 +150,6 @@
                         int totalScore = leftScore * rightScore * topScore * bottomScore;
                         if (totalScore > visibleCount)
                         {
-                            Console.WriteLine(leftScore + " - " + rightScore + " - " + topScore + " - " + bottomScore);
-                            Console.WriteLine(column + " - " + row);
-                            Console.WriteLine(forrest[column][row]);
                             visibleCount = totalScore;
                         }
                     }
